a40d0710f3d40decd7ae91156d71e3ced4fac0df
ambitsite <yfljvtymitgbnmrtif@yandex.ru>
2020-05-26 21:34:48 +0300

    fix

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var filterBtn = document.querySelectorAll('.filters-item')
+
+for(var countFilterBtn = 0;countFilterBtn<filterBtn.length;countFilterBtn++){
+    filterBtn[countFilterBtn].addEventListener('click', function(e){
+        var filterBtnActive = document.querySelector('.filters-item_active')
+        filterBtnActive.classList.remove('filters-item_active')
+        e.target.classList.add('filters-item_active')
+    })
+}
+
+$(document).ready(function () {
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+});
