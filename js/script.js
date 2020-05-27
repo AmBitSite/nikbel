b4209d31071878b95df7e379a5047b6eddbff597
ambitsite <yfljvtymitgbnmrtif@yandex.ru>
2020-05-27 22:00:10 +0300

    add anchor to link

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var filterBtn = document.querySelectorAll('.filters-item')
 
-for(var countFilterBtn = 0;countFilterBtn<filterBtn.length;countFilterBtn++){
-    filterBtn[countFilterBtn].addEventListener('click', function(e){
+for (var countFilterBtn = 0; countFilterBtn < filterBtn.length; countFilterBtn++) {
+    filterBtn[countFilterBtn].addEventListener('click', function (e) {
         var filterBtnActive = document.querySelector('.filters-item_active')
         filterBtnActive.classList.remove('filters-item_active')
         e.target.classList.add('filters-item_active')
@@ -22,5 +22,14 @@ $(document).ready(function () {
         image: {
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
+    });
+
+});
+$(document).ready(function () {
+    $("#menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 500);
     });
 });
