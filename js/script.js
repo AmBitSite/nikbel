766bd926bfe1211209e28228cb501f4256747c41
ambitsite <yfljvtymitgbnmrtif@yandex.ru>
2020-06-01 21:15:25 +0300

    add scroll to anchor

--- js/script.js
+++ js/script.js
@@ -32,4 +32,13 @@ $(document).ready(function () {
             top = $(id).offset().top;
         $('body,html').animate({ scrollTop: top }, 500);
     });
+
+});
+$(document).ready(function () {
+    $("#menu1").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 500);
+    });
 });
